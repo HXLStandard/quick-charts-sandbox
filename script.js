f9c1606a9a71d16f96d07ea90dfddee27a558894
David Megginson <david.megginson@megginson.com>
2018-10-04 15:12:35 -0400

    Update to use GET params when available, and wrap in window.onload

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
+/**
+ * Script to run on page load
+ */
 
-var config_pattern = '{"configVersion":2,"bites": [{{BITE}}]}';
-var preview_url_pattern = "https://data.humdata.org/hxlpreview/show;url={{URL}};embeddedConfig={{CONFIG}};singleWidgetMode=true";
+// Constant patterns
+var CONFIG_PATTERN = '{"configVersion":2,"bites": [{{BITE}}]}';
+var URL_PATTERN = "https://data.humdata.org/hxlpreview/show;url={{URL}};embeddedConfig={{CONFIG}};singleWidgetMode=true";
 
-function handle_submit () {
-    event.preventDefault();
-    update_preview();
-    return false;
-}
+/**
+ * Set up on page load
+ */
+window.onload = function () {
 
-function update_preview(url, bite) {
-    var url = document.getElementById('url').value;
-    var bite = document.getElementById('bite').value;
-    var config = config_pattern
-        .replace('{{BITE}}', bite.replace(/("[^"]*")|\s/g, "$1"));
-    var preview_url = preview_url_pattern
-        .replace('{{URL}}', encodeURIComponent(url))
+    // GET query parameters
+    var url_params = new URLSearchParams(window.location.search);
+    var data_url = url_params.get('url');
+    var json_bite = url_params.get('bite');
+
+    // get data URL from query param or web page
+    if (data_url) {
+        document.getElementById('url').value = data_url;
+    } else {
+        data_url = document.getElementById('url').value;
+    }
+
+    // get JSON bite from query param or web page
+    if (json_bite) {
+        document.getElementById('bite').value = json_bite;
+    } else {
+        json_bite = document.getElementById('bite').value;
+    }
+
+
+    // embedded JSON
+    var config = CONFIG_PATTERN
+        .replace('{{BITE}}', json_bite.replace(/("[^"]*")|\s/g, "$1"));
+
+    // entire Quick Charts URL
+    var preview_url = URL_PATTERN
+        .replace('{{URL}}', encodeURIComponent(data_url))
         .replace('{{CONFIG}}', encodeURIComponent(config));
+
+    // set up the iFrame
     document.getElementById('preview').setAttribute('src', preview_url);
     document.getElementById('embed-url').textContent = preview_url;
     document.getElementById('embed-url').setAttribute('href', preview_url);
-}
-
-update_preview();
-
-document.getElementById('config').addEventListener('submit', handle_submit);
+};
